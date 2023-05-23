7a4ebe5fad1ea55907f76f6742adab06a61dd809
Anton Ganhammar <ganhammar@gmail.com>
2023-05-23 08:40:30 +0200

    fix: revert

--- src/Example/Function.cs
+++ src/Example/Function.cs
@@ -1,7 +1,6 @@
 using Amazon.Lambda.Core;
 using Amazon.Lambda.Serialization.SystemTextJson;
 using AWS.Lambda.Powertools.Logging;
-using Datadog.Trace;
 using Datadog.Trace.Annotations;
 using DotnetDatadog.Shared;
 using Microsoft.Extensions.DependencyInjection;
@@ -32,17 +31,11 @@ public class Function : FunctionBase<Request, Result>
   [Trace(OperationName = "example.do-heavy-thinking", ResourceName = "Example.Think")]
   private async Task Think()
   {
-    using (var scope = Tracer.Instance.StartActive("example.do-heavy-thinking-in-scope"))
-    {
-      scope.Span.ResourceName = "Example.ThinkInScope";
-      var random = new Random();
-      var thinkingTime = random.Next(50, 1000);
+    var random = new Random();
+    var thinkingTime = random.Next(50, 1000);
 
-      scope.Span.SetTag("thinking.time", $"{thinkingTime}ms");
+    Logger.LogInformation($"Need to think for {thinkingTime}ms");
 
-      Logger.LogInformation($"Need to think for {thinkingTime}ms");
-
-      await Task.Delay(thinkingTime);
-    }
+    await Task.Delay(thinkingTime);
   }
 }
